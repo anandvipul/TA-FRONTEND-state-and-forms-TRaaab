2ba0a98b4e01f4fbb8b22b5028b9ec6c1a16b6ce
anandvipul <anandvipul01@gmail.com>
2023-03-01 21:38:37 +0530

    handling State

---
+++ block-BRaabd/handling-state/src/Components/CategoriesItem.js
@@ -0,0 +1,10 @@
+let CategoryItem = {
+  BASKETBALL: "/basketball.jpg",
+  PUBG: "/pubg.jpeg",
+  TIGER: "/tiger.jpg",
+  PHONE: "/phone.jpg",
+  LAPTOP: "/laptop.jpg",
+  CRICKET: "/cricket.jpg",
+};
+
+export default CategoryItem;
