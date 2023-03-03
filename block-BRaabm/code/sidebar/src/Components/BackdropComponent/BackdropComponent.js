41c4fddd349cdb429516f29f1ec66cb4b2ad7bae
anandvipul <anandvipul01@gmail.com>
2023-03-03 20:15:15 +0530

    Sidebar

---
+++ block-BRaabm/code/sidebar/src/Components/BackdropComponent/BackdropComponent.js
@@ -0,0 +1,14 @@
+function BackdropComponent(props) {
+  return (
+    <>
+      <div className="backdrop-movie">
+        <span className="backdrop-movie-title">Single Movie</span>
+        <span className="backdrop-movie-close" onClick={() => {}}>
+          Close
+        </span>
+      </div>
+    </>
+  );
+}
+
+export default BackdropComponent;
