1cb9e77632e767c9af807b67909bc2b3610cd737
anandvipul <anandvipul01@gmail.com>
2023-03-04 01:38:45 +0530

    Form

---
+++ block-BRaabr/code/form-validation/src/Components/Container/Container.js
@@ -0,0 +1,128 @@
+import React from "react";
+
+class Container extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+      date: "",
+      file: "",
+      readOnlyInput: "",
+      disabledInput: "Disabled Input",
+      textarea: "",
+      textareaDisabled: "TextArea Disabled",
+    };
+    this.fileInput = React.createRef();
+  }
+
+  handleChange = (event) => {
+    event.preventDefault();
+    let type = event.target.name;
+
+    if (type !== "file") {
+      console.log(`[${event.target.name}]: ${event.target.value}`);
+      this.setState((prevState) => {
+        return {
+          [`${event.target.name}`]: event.target.value,
+        };
+      });
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <form>
+          <div className="form-item">
+            <label htmlFor="text" />
+            Text Input
+            <input
+              type="text"
+              id="text"
+              name="text"
+              value={this.state.text}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div className="form-item">
+            <label htmlFor="date" />
+            Date Input
+            <input
+              type="date"
+              id="date"
+              name="date"
+              value={this.state.date}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div className="form-item">
+            <label htmlFor="file" />
+            File Input
+            <input
+              ref={this.fileInput}
+              type="file"
+              id="file"
+              name="file"
+              value={this.state.file}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div className="form-item">
+            <label htmlFor="readonly-text" />
+            Read-Only input
+            <input
+              type="text"
+              id="readonly-text"
+              name="readonly-text"
+              value={this.state.readOnlyInput}
+              readOnly
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div className="form-item">
+            <label htmlFor="disabled-text" />
+            Disabled Input
+            <input
+              type="text"
+              id="disabled-text"
+              name="disabled-text"
+              value={this.state.disabledInput}
+              disabled
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div className="form-item">
+            <label htmlFor="textarea" />
+            Textarea
+            <textarea
+              type="textarea"
+              id="textarea"
+              name="textarea"
+              value={this.state.textArea}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div className="form-item">
+            <label htmlFor="textarea-disabled" />
+            Textarea Disabled
+            <textarea
+              type="textarea"
+              id="textarea-disabled"
+              name="textarea-disabled"
+              value={this.state.textareaDisabled}
+              onChange={this.handleChange}
+            />
+          </div>
+        </form>
+      </>
+    );
+  }
+}
+
+export default Container;
